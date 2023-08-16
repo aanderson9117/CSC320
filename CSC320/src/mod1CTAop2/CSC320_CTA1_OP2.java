3d4ea8647e7e132bb85d9a092c83c60102adc21c
aanderson9117 <alyse.anderson@csuglobal.edu>
2023-08-16 16:59:17 -0400

    CSC320 Module 1 CTA 1 Option 2

---
+++ CSC320/src/mod1CTAop2/CSC320_CTA1_OP2.java
@@ -0,0 +1,12 @@
+package mod1CTAop2;
+
+public class CSC320_CTA1_OP2 {
+
+	public static void main(String[] args) {
+		System.out.println("Resturant Name: Burnin 99");
+		System.out.println("Buisness Address: ");
+		System.out.println("7620 Rivers Ave St #320");
+		System.out.println("North Charleston, SC, 29406");
+	}
+
+}
